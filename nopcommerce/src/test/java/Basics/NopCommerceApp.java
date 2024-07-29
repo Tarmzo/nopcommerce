a8d8ae20f44e8f71c4b003c196d907e2f5bde922
unknown <Avuyile Malotana>
2024-07-29 21:33:18 +0200

    nitial commit

---
+++ nopcommerce/src/test/java/Basics/NopCommerceApp.java
@@ -0,0 +1,9 @@
+package Basics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NopCommerceApp {
+
+    WebDriver webDriver = new ChromeDriver();
+}
