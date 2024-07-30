e9ed1465c8197ba969184b9a5886fb2003b9c577
Avuyile Malotana <vlvsmalotana@gmail.com>
2024-07-30 22:36:52 +0200

    Added new files and Test generate

--- nopcommerce/src/test/java/Basics/NopCommerceApp.java
+++ nopcommerce/src/test/java/Basics/NopCommerceApp.java
@@ -1,9 +1,39 @@
 package Basics;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
 
 public class NopCommerceApp {
 
-    WebDriver webDriver = new ChromeDriver();
+    WebDriver webDriver;
+
+    @Test
+    public void LoginTest() {
+        webDriver = new ChromeDriver();
+
+        webDriver.manage().window().maximize();
+        webDriver.get("https://demo.nopcommerce.com/register?returnUrl=%2F"); // This line start the browser
+        webDriver.findElement(By.id("gender-male")).click();
+        webDriver.findElement(By.id("FirstName")).sendKeys("Enzokuhle");
+        webDriver.findElement(By.id("LastName")).sendKeys("Malotana");
+        webDriver.findElement(By.name("DateOfBirthDay")).sendKeys("29");
+        webDriver.findElement(By.name("DateOfBirthMonth")).sendKeys("July");
+        webDriver.findElement(By.name("DateOfBirthYear")).sendKeys("2017");
+        webDriver.findElement(By.id("Email")).sendKeys("enzoklesac@malotana.com");
+        webDriver.findElement(By.id("Company")).sendKeys("FLASH");
+        webDriver.findElement(By.id("Password")).sendKeys("Password123");
+        webDriver.findElement(By.id("ConfirmPassword")).sendKeys("Password123");
+        webDriver.findElement(By.id("register-button")).click();
+
+        //Assert.assertEquals(registerResults, "Products");
+    }
+
+    @AfterTest
+    public void closeBrowser(){
+        webDriver.quit();
+    }
 }
